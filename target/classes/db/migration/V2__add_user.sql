c172639135e4b7643b20d7d0679d50e7fa129d42
tomakliskinic <tomakliskinic123@gmail.com>
2025-10-29 14:14:46 +0100

    API and Docker test

---
+++ target/classes/db/migration/V2__add_user.sql
@@ -0,0 +1,19 @@
+/*
+INSERT INTO users (
+    username,
+    email,
+    password_hash,
+    first_name,
+    last_name,
+    role,
+    date_of_birth
+) VALUES (
+    'jdoe',
+    'jdoe@example.com',
+    '$2a$10$7fW1zKqWjD5m7s8R4T6i2uQhZ9h8gF6h3T5yP9rE0uVfB7cT1lOQy', -- example bcrypt hash
+    'John',
+    'Doe',
+    'student',
+    '2000-05-15'
+);
+*/
