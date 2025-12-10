4a0b5fed5a80f321b4b3482b3f89aa0efd6fb513
LukaJurinovic <lukajurinovic2701@gmail.com>
2025-12-10 17:19:38 +0100

    refactor: domain/business separation; enrollment flow; auth/validation; DTO updates

---
+++ src/main/java/org/example/domain/dto/CourseInfo.java
@@ -0,0 +1,4 @@
+package org.example.service.business;
+
+public class CourseInfo {
+}
--- src/main/java/org/example/domain/dto/DashboardResponse.java
+++
@@ -1,9 +0,0 @@
-package org.example.domain.dto;
-
-public record DashboardResponse(
-        Long id,
-        String email,
-        String firstName,
-        String lastName,
-        Integer role
-) {}
